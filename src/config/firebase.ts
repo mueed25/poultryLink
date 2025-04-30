47e5b2fa8417baf38a01f5903143df149ff58af3
mueed25 <109614264+mueed25@users.noreply.github.com>
2025-04-30 10:55:14 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,15 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // 1) Firebase config from console
-const firebaseConfig = {
-  apiKey: "AIzaSyBLYK5uJRrWD4z7J7Hk-daWRntETQgbkKE",
-  authDomain: "poultrypro-98ef7.firebaseapp.com",
-  projectId: "poultrypro-98ef7",
-  storageBucket: "poultrypro-98ef7.firebasestorage.app",
-  messagingSenderId: "613206992878",
-  appId: "1:613206992878:web:5600636e00e8161b4fd0ff",
-  measurementId: "G-5BRB32WFDN"
-};
+
 
 // 2) Initialize app
 const app = initializeApp(firebaseConfig);
